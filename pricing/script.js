956686145ef680972a69886c7a2b26bf17b7bd21
lucascr91 <lucas.ecomg@gmail.com>
2020-11-14 17:06:16 -0300

    Update pricing

--- pricing/script.js
+++ pricing/script.js
@@ -1,28 +1,42 @@
-class Budget {
-    constructor(pages, kind, grammar, f_ref, f_all, style) {
-        this.pages=pages
-        this.kind=kind
-        this.grammar=grammar
-        this.f_ref=f_ref
-        this.f_all=f_all
-        this.style=style
-        this.clear()
-    }
-
-    clear() {
-        this.result = ''
-    }
-
-    compute () {
-
-    }
+function reset() {
+    document.querySelector('.result').innerHTML = "0,00"
 }
 
-var user_pages=document.getElementById("pages").value
-var user_kind=document.getElementById("pages").value
-var user_grammar=document.getElementById("pages").value
-var user_fref=document.getElementById("pages").value
-var user_fall=document.getElementById("pages").value
-var user_style=document.getElementById("pages").value
+for (k in fields) {
+    console.log(k)
+}
 
+
+function  budget() {
+    var kind=document.getElementById("arbeit").value
+    var pages=document.getElementById("pages").value
+    var grammar=document.getElementById("grammar").value
+    var f_ref=document.getElementById("fix_ref").value
+    var f_all=document.getElementById("fix_all").value
+    var style=document.getElementById("style_ref").value
+    var doc=document.getElementById("kind_doc").value
+
+    var fields=[kind,pages, grammar, f_ref, f_all, style, doc]
+
+    if (pages=='' || kind=='') {
+        alert("Ao menos 3 campos devem ser preenchidos. Sendo \"Número de laudas\" e \"Tipo de Trabalho\" sempre obrigatórios.")
+
+    } else {
+        if (kind=="monographics") {
+            document.querySelector('.result').innerHTML = 2*pages
+        }
+        else if (kind=="dissertation") {
+            document.querySelector('.result').innerHTML = 3*pages
+        }
+        else if (kind=="thesis") {
+            document.querySelector('.result').innerHTML = 4*pages
+        }
+        else if (kind=="article") {
+            document.querySelector('.result').innerHTML = 5*pages
+        }
+        else {
+            document.querySelector('.result').innerHTML = "Something goes wrong"
+        }
+    }
+}
 
